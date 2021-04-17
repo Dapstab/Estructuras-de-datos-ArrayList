bc66a2893ff5a5c5a2b43ad4de2c6b180e7dc3a6
MADAMEMO <anguipesi@gmail.com>
2021-04-17 17:21:28 -0500

    Pilas usando un array

--- src/js/dataStructures.js
+++ src/js/dataStructures.js
@@ -40,6 +40,30 @@ export class SinglyLinkedList {
     }
   }
 }
+export class Stack{
+  constructor(){
+    this.Stack = [];
+  }
+
+  push(element){
+    this.stack.push(element);
+    return this.stack;
+  }
+
+  pop(){
+    return this.stack.pop();
+  }
+
+  peek(){
+    return this.stack[this.stack.length -1];
+  }
+  size(){
+    return this.stack.length
+  }
+  print(){
+    console.log(this.stack);
+  }
+}
 
 export default new SinglyLinkedList();
 console.log("hola");
